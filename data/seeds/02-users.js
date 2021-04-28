41dc90ef446d464f5298bb52aeae707a71bd4d9b
cameronyoung94 <68090343+cameronyoung94@users.noreply.github.com>
2021-04-27 21:16:57 -0400

    changed hash numbers in the seed files

--- data/seeds/02-users.js
+++ data/seeds/02-users.js
@@ -5,17 +5,17 @@ exports.seed = async function(knex) {
    await knex('users').insert([
      {
          username: 'Joe Smith',
-         password:  await bcrypt.hash("Lambda1", 12),
+         password:  await bcrypt.hash("Lambda1", 8),
          phone_number: '111-111-1111' 
      },
      {
          username: 'Mary Smith',
-         password:  await bcrypt.hash("Lambda1", 12),
+         password:  await bcrypt.hash("Lambda1", 8),
          phone_number: '222-222-2222' 
      },
      {
          username: 'Kim Smith',
-         password:  await bcrypt.hash("Lambda1", 12),
+         password:  await bcrypt.hash("Lambda1", 8),
          phone_number: '333-333-3333' 
      }
    ]);
